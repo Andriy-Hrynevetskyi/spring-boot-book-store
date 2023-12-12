03f8fba846dc6c37541cb37f228855954f83a229
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-12 18:51:38 +0200

    added searching feature to this project, all needed interfaces and methods were implemented

--- src/main/java/bookstore/repository/book/BookRepository.java
+++ src/main/java/bookstore/repository/book/BookRepository.java
@@ -1,8 +1,12 @@
-package bookstore.repository;
+package bookstore.repository.book;
 
 import bookstore.model.Book;
+import java.util.List;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsBookById(Long id);
+
+    List<Book> findAll(Specification<Book> bookSpecification);
 }
