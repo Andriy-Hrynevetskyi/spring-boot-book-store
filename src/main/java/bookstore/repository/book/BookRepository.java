3025087a4f02dd2fbbcbdfa245a1b2480663e3be
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-19 14:56:26 +0200

    added Pageable class for pagination

--- src/main/java/bookstore/repository/book/BookRepository.java
+++ src/main/java/bookstore/repository/book/BookRepository.java
@@ -2,6 +2,9 @@ package bookstore.repository.book;
 
 import bookstore.model.Book;
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/bookstore/service/BookService.java
+++ src/main/java/bookstore/service/BookService.java
@@ -3,12 +3,14 @@ package bookstore.service;
 import bookstore.dto.BookDto;
 import bookstore.dto.BookSearchParameters;
 import bookstore.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
