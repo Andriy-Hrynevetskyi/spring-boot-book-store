1cb8008ef465a37349544e826b8524b8ebf5d2a3
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-19 12:15:42 +0200

    checkstyle fix

--- src/main/java/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,11 @@
 package bookstore.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.math.BigDecimal;
-
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
@@ -19,7 +17,7 @@ public class CreateBookRequestDto {
     @Size(max = 255)
     private String author;
     @NotBlank(message = "ISBN field can't be null or empty")
-    @Pattern(regexp="\\d{13}", message = "isbn must contain 13 digits")
+    @Pattern(regexp = "\\d{13}", message = "isbn must contain 13 digits")
     private String isbn;
     @NotNull(message = "Price can't be null or less than 0")
     @Positive
--- src/main/java/bookstore/repository/book/BookSpecificationBuilder.java
+++ src/main/java/bookstore/repository/book/BookSpecificationBuilder.java
@@ -7,7 +7,6 @@ import bookstore.repository.SpecificationProviderManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
-//import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -30,42 +29,5 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
                     .getSpecification(searchParameters.prices()));
         }
         return spec;
-
-        /*return Optional.of(Specification.where(null))
-                .map(spec -> addAuthorSpecification(spec, searchParameters))
-                .map(spec -> addTitleSpecification(spec, searchParameters))
-                .map(spec -> addPriceSpecification(spec, searchParameters))
-                .orElse(Specification.where(null));
-    }
-
-    private Specification<Book> addPriceSpecification(
-    Specification<Book> spec,
-    BookSearchParameters searchParameters) {
-        return Optional.ofNullable(searchParameters.prices())
-                .filter(prices -> prices.length > 0)
-                .map(prices -> spec.and(bookSpecificationProviderManager
-                .getSpecificationProvider("price")
-                .getSpecification(prices)))
-                .orElse(spec);
-    }
-
-    private Specification<Book> addTitleSpecification(Specification<Book> spec,
-    BookSearchParameters searchParameters) {
-        return Optional.ofNullable(searchParameters.prices())
-                .filter(titles -> titles.length > 0)
-                .map(titles -> spec.and(bookSpecificationProviderManager
-                .getSpecificationProvider("title")
-                .getSpecification(titles)))
-                .orElse(spec);
-    }
-
-    private Specification<Book> addAuthorSpecification(Specification<Book> spec,
-    BookSearchParameters searchParameters) {
-        return Optional.ofNullable(searchParameters.prices())
-                .filter(authors -> authors.length > 0)
-                .map(authors -> spec.and(bookSpecificationProviderManager
-                .getSpecificationProvider("author")
-                .getSpecification(authors)))
-                .orElse(spec);*/
     }
 }
