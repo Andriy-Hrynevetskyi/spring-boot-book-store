4ce4a4ed8183ffd0ab13d90e9feeb1bc8cc870b7
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-19 12:11:16 +0200

    improved dto validation after code review

--- src/main/java/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -4,22 +4,25 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    private static final String TITLE_ERR_MSG = "Title field can't be null or empty";
-    private static final String AUTHOR_ERR_MSG = "Author field can't be null or empty";
-    private static final String ISBN_ERR_MSG = "ISBN field can't be null or empty";
-    private static final String PRICE_ERR_MSG = "Price can't be null or less than 0";
-    @NotBlank(message = TITLE_ERR_MSG)
+    @NotBlank(message = "Title field can't be null or empty")
+    @Size(max = 255)
     private String title;
-    @NotBlank(message = AUTHOR_ERR_MSG)
+    @NotBlank(message = "Author field can't be null or empty")
+    @Size(max = 255)
     private String author;
-    @NotBlank(message = ISBN_ERR_MSG)
+    @NotBlank(message = "ISBN field can't be null or empty")
+    @Pattern(regexp="\\d{13}", message = "isbn must contain 13 digits")
     private String isbn;
-    @NotNull(message = PRICE_ERR_MSG)
-    @Min(0)
+    @NotNull(message = "Price can't be null or less than 0")
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
