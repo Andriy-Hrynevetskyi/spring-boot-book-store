5892ee52d6756fbf6e50ca209b61773ca93ec4f7
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-21 20:31:43 +0200

    added UserDetailsService and its implementation added UserDetails

--- src/main/java/bookstore/security/CustomUserDetailsService.java
+++ src/main/java/bookstore/security/CustomUserDetailsService.java
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find user by email"));
     }
 }
