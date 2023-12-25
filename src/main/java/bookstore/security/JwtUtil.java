badb011191f889ad1be6f2267efe9cf98dfc36d2
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-25 18:33:06 +0200

    added new dtos, auth service and jwt util, login endpoint

---
+++ src/main/java/bookstore/security/JwtUtil.java
@@ -0,0 +1,30 @@
+package bookstore.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    private final Key secret;
+    @Value("${jwt.expiration:300000}")
+    private Long expiration;
+
+    public JwtUtil(@Value(
+            "${jwt.secret:someDefaultStringWithRecommendedLength}") String secretString) {
+        this.secret = Keys.hmacShaKeyFor(secretString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secret)
+                .compact();
+    }
+}
