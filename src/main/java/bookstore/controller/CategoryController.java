31abff8f331ee7d1d4143b02d7235a90f6fdf3fc
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-28 17:18:16 +0200

    added open api annotations and security permissions

--- src/main/java/bookstore/controller/CategoryController.java
+++ src/main/java/bookstore/controller/CategoryController.java
@@ -5,10 +5,13 @@ import bookstore.dto.category.CategoryDto;
 import bookstore.dto.category.CategoryRequestDto;
 import bookstore.service.book.BookService;
 import bookstore.service.category.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,38 +24,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/categories")
+@Tag(name = "Category management", description = "Endpoints for managing categories")
 public class CategoryController {
     private final CategoryService categoryService;
     private final BookService bookService;
 
     @GetMapping
+    @Operation(summary = "Get all categories", description = "Get all available categories")
     public List<CategoryDto> getAll(Pageable pageable) {
         return categoryService.getAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find category by id", description = "Get category by id if it is valid")
     public CategoryDto getCategoryById(@PathVariable Long id) {
         return categoryService.getById(id);
     }
 
     @GetMapping("/{id}/books")
+    @Operation(summary = "Find books by its category id", description = "Get all books by category")
     public List<BookWithoutCategoryIds> getAllBooksByCategoryId(Pageable pageable,
                                                                 @PathVariable Long id) {
         return bookService.findAllByCategoryId(pageable, id);
     }
 
     @PostMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Operation(summary = "Create a new category",
+            description = "Add a new category to DB if input data is valid")
     public CategoryDto createCategory(@RequestBody @Valid CategoryRequestDto requestDto) {
         return categoryService.save(requestDto);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Operation(summary = "Update a category by id",
+            description = "Updates the category with given id if all data is valid")
     public void updateCategoryById(@PathVariable Long id,
                                    @RequestBody @Valid CategoryRequestDto requestDto) {
         categoryService.updateCategoryById(id, requestDto);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Operation(summary = "Delete a category by id",
+            description = "Set the is_deleted parameter 'true' if id is valid")
     public void deleteCategoryById(@PathVariable Long id) {
         categoryService.deleteCategoryById(id);
     }
