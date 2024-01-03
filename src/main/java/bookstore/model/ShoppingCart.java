e0289a16aec1e582763f0185e34dad0f8a89f978
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2024-01-04 01:58:39 +0200

    added shopping cart and cart items entities and changelog files

---
+++ src/main/java/bookstore/model/ShoppingCart.java
@@ -0,0 +1,43 @@
+package bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "shopping_carts")
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private User user;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "shoppingCart")
+    private Set<CartItem> cartItems;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
