60a6465c06f52b5c6a5b6604ea13e377d2ddc9b4
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2024-01-08 15:32:26 +0200

    added preauthorize annotations

--- src/main/java/bookstore/controller/OrderController.java
+++ src/main/java/bookstore/controller/OrderController.java
@@ -15,9 +15,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +32,7 @@ public class OrderController {
 
     @PostMapping
     @Operation(summary = "Add a new order")
+    @PreAuthorize("hasRole('USER')")
     public OrderDto addOrder(@RequestBody AddOrderRequestDto requestDto,
                              Authentication authentication) {
         User user = (User)authentication.getPrincipal();
@@ -40,12 +41,14 @@ public class OrderController {
 
     @GetMapping
     @Operation(summary = "Get user's orders history")
+    @PreAuthorize("hasRole('USER')")
     public List<OrderDto> getAllOrders(Authentication authentication, Pageable pageable) {
         User user = (User)authentication.getPrincipal();
         return orderService.getAllOrders(user.getId(), pageable);
     }
 
     @GetMapping("/{orderId}/items")
+    @PreAuthorize("hasRole('USER')")
     @Operation(summary = "Get item list of certain order")
     public List<OrderItemDto> getOrderDetails(@PathVariable Long orderId,
                                               Authentication authentication,
@@ -55,6 +58,7 @@ public class OrderController {
     }
 
     @GetMapping("/{orderId}/items/{id}")
+    @PreAuthorize("hasRole('USER')")
     @Operation(summary = "Get certain order item")
     public OrderItemDto getOrderItem(@PathVariable Long orderId,
                                      @PathVariable Long id,
@@ -63,7 +67,7 @@ public class OrderController {
         return orderItemService.getOrderItemByOrderId(user.getId(), orderId, id);
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping ("/{id}")
     @Operation(summary = "Update order status. ADMIN only")
     @PreAuthorize("hasRole('ADMIN')")
     public OrderDto updateOrderStatus(@PathVariable Long id,
